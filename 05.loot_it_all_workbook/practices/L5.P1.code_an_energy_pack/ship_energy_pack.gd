64ca6aa796467b11c488b1ee8fced35d4fcc0ff7
Celine <hewong4@s.sfusd.edu>
2024-04-05 11:28:37 -0700

    M5 L5 practice 1

--- 05.loot_it_all_workbook/practices/L5.P1.code_an_energy_pack/ship_energy_pack.gd
+++ 05.loot_it_all_workbook/practices/L5.P1.code_an_energy_pack/ship_energy_pack.gd
@@ -2,13 +2,14 @@ extends Area2D
 
 var max_speed := 1200.0
 var velocity := Vector2(0, 0)
-var steering_factor := 10.0
+var steering_factor := 3.0
 
 var energy := 20.0
 
 
 func _ready() -> void:
-	get_node("UI/EnergyBar").value = energy
+	area_entered.connect(_on_area_entered)
+	set_energy(20)
 
 
 func _process(delta: float) -> void:
@@ -20,8 +21,11 @@ func _process(delta: float) -> void:
 	position += velocity * delta
 
 	if velocity.length() > 0.0:
-		get_node("Sprite2D").rotation = velocity.angle()
+			get_node("Sprite2D").rotation = velocity.angle()
 
+func set_energy(new_energy: int) -> void:
+	energy = new_energy
+	get_node("UI/EnergyBar").value = energy
 
 func _on_area_entered(area: Area2D) -> void:
-	pass
+	set_energy(energy + 20)
